b000fd01cf7765bcfee431a6f759aaf208d37e22
Altun0 <91114236+Altun0@users.noreply.github.com>
2021-12-21 00:14:41 +0400

    commit 17

--- progress-bar.js
+++ progress-bar.js
@@ -1,1 +1,165 @@
 
+
+update()
+function update() {
+var element = document.getElementById("myprogressBar");
+var width = 1;
+var identity = setInterval(scene, 15);
+function scene() {
+	if (width >= 95) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	element.innerHTML = width * 1 + '%';
+	}
+}
+}
+
+update2()
+function update2() {
+var element = document.getElementById("myprogressBar2");
+var width = 1;
+var identity = setInterval(scene, 15);
+function scene() {
+	if (width >= 85) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	element.innerHTML = width * 1 + '%';
+	}
+}
+}
+
+update3()
+function update3() {
+var element = document.getElementById("myprogressBar3");
+var width = 1;
+var identity = setInterval(scene, 15);
+function scene() {
+	if (width >= 60) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	element.innerHTML = width * 1 + '%';
+	}
+}
+}
+
+update4()
+function update4() {
+var element = document.getElementById("myprogressBar4");
+var width = 1;
+var identity = setInterval(scene, 15);
+function scene() {
+	if (width >= 95) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	element.innerHTML = width * 1 + '%';
+	}
+}
+}
+
+update5()
+function update5() {
+var element = document.getElementById("myprogressBar5");
+var width = 1;
+var identity = setInterval(scene, 15);
+function scene() {
+	if (width >= 80) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	element.innerHTML = width * 1 + '%';
+	}
+}
+}
+
+update6()
+function update6() {
+var element = document.getElementById("myprogressBar6");
+var width = 1;
+var identity = setInterval(scene, 15);
+function scene() {
+	if (width >= 66) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	element.innerHTML = width * 1 + '%';
+	}
+}
+}
+
+update7()
+function update7() {
+var element = document.getElementById("myprogressBar6");
+var width = 1;
+var identity = setInterval(scene, 15);
+function scene() {
+	if (width >= 80) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	element.innerHTML = width * 1 + '%';
+	}
+}
+}
+
+update8()
+function update8() {
+var element = document.getElementById("myprogressBar8");
+var width = 1;
+var identity = setInterval(scene, 15);
+function scene() {
+	if (width >= 90) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	element.innerHTML = width * 1 + '%';
+	}
+}
+}
+
+update9()
+function update9() {
+var element = document.getElementById("myprogressBar9");
+var width = 1;
+var identity = setInterval(scene, 15);
+function scene() {
+	if (width >= 70) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	element.innerHTML = width * 1 + '%';
+	}
+}
+}
+
+update10()
+function update10() {
+var element = document.getElementById("myprogressBar10");
+var width = 1;
+var identity = setInterval(scene, 15);
+function scene() {
+	if (width >= 50) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	element.innerHTML = width * 1 + '%';
+	}
+}
+}
+
+
+
+
